6487b5054f72aec110a8480c1cd51b41aad054f1
Dyana George <dyana.t.s.george@gmail.com>
2018-12-02 03:52:57 -0500

    reinitialized files

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-server',
+	templateUrl: '<app-server></app-server>',
+	styleUrls: ['./servers.component.css']
+})
+export class ServerComponent {
+
+	constructor() {}
+
+	ngOnInit() {}
+
+	
+}
